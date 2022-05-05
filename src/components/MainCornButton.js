728189d617149fc99f2c1a307ebc5745fe0ad235
DingoDev <randilea@outlook.com>
2022-05-05 16:48:21 -0600

    Finished main page

--- src/components/MainCornButton.js
+++ src/components/MainCornButton.js
@@ -3,13 +3,25 @@ import React, { component } from "react";
 import { Button, Row, Col } from "react-bootstrap";
 import "../App.css";
 
-const CornEmojis = () => {
+const CornEmojis = ({ onClick }) => {
+  // <link href="https://fonts.googleapis.com/css?family=Oswald:600,700" rel="stylesheet">
+  <div id="all">
+    <div id="container">
+      <div id="animate"></div>
+    </div>
+  </div>;
+
   return (
     <Row>
       <Col></Col>
       <Col></Col>
       <Col xs lg="4">
-        <Button className="emojiButton" variant="outline-info" size="sm">
+        <Button
+          className="emojiButton"
+          variant="outline-info"
+          size="sm"
+          onClick={onClick}
+        >
           his FAVORITE things
         </Button>
       </Col>
