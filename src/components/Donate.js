7662fff27ca9c10d7c1af16e25a166ddc0c3e9d2
DingoDev <randilea@outlook.com>
2022-07-05 17:24:24 -0600

    Working through donation page

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -3,7 +3,75 @@ import { Row, Col } from "react-bootstrap";
 const Donation = () => {
   return (
     <div>
-      <h1>This is the donation page.</h1>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col xs lg="10">
+          {bookingHeader()}
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col xs lg="4">
+          <div id="animate" />
+          {bookingSubline()}
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col xs lg="8">
+          {bookingParagraph()}
+        </Col>
+        <Col></Col>
+      </Row>
+    </div>
+  );
+};
+
+const bookingHeader = () => {
+  return (
+    <div>
+      <h3 className="donateHeader">
+        Want to help Cricket but don't need to schedule an appointment?
+      </h3>
+    </div>
+  );
+};
+
+const bookingSubline = () => {
+  return (
+    <div>
+      <h5 className="donateSubline">Make a donation!</h5>
+    </div>
+  );
+};
+
+const bookingParagraph = () => {
+  return (
+    <div>
+      <p className="donateParagraph">
+        Donations go toward keeping Cricket healthy and happy. Dogs are
+        expensive, especially when they're as popular and active as he is! Every
+        dollar sent will go directly toward vet bills, food, treats, healthy
+        chews, plush toys, puzzle toys, grooming supplies, supplements, and
+        bedding.
+      </p>
+      <p className="donateParagraph">
+        You are welcome to donate a random amount, big or small (every little
+        bit helps!), or the exact price for some of his favorite things. For a
+        list of top suggestions and their prices, you can email
+        CricketWishlist@outlook.com. We'll respond as quickly as possible!
+      </p>
+      <p className="donateParagraph">
+        Donations also make wonderful tips if you recently book a session with
+        Cricket and loved his services! He's a dog, so he has no use for money
+        by itself, but a Fluff 'n Tuff toy? That's well-earned for a job
+        well-done.
+      </p>
+      <p className="donateParagraph">Cricket thanks you!</p>
     </div>
   );
 };
