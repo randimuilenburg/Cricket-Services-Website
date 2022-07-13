bf1c4794f2fb889404f4f42eaa9e3379fe7f1a22
DingoDev <randilea@outlook.com>
2022-07-13 14:40:04 -0600

    Adjusted server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const port = process.env.PORT || 3000;
 const path = require("path");
+const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
